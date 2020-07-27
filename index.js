0e90664e395718272d190324848cfce13672df15
TyTy <Tylieafter1@gmail.com>
2020-07-27 17:01:14 +0000

    I made an student object

---
+++ index.js
@@ -0,0 +1,16 @@
+// my object
+var student = {
+  firstName: "Princess",
+  lastName: "Fionna",
+  age: 50,
+  eyeColor: "blue",
+  grades: "graduated",
+  address: "The Swamp",
+  parents: "king Harold and queen Lillian",
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+var car = {type:"Fiat", model:"500", color:"white"};
+
+
