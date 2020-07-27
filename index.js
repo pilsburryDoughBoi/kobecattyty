5750fa429f6208f137675a10f4cd25b55c20ef39
TyTy <Tylieafter1@gmail.com>
2020-07-27 17:09:08 +0000

    I added a function that determines the student's gpa

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ var student = {
   lastName: "Fionna",
   age: 50,
   eyeColor: "blue",
-  grades: "graduated",
+  race: "oger/human"
+  gradelvl: "graduated",
   address: "The Swamp",
   parents: "king Harold and queen Lillian",
-  fullName : function() {
-    return this.firstName + " " + this.lastName;
-  }
+  freshman: 4.0,
+  sophmore: 4.87,
+  junior: 5.30,
+  senior: 6.8,
+  function cumulativeGPA (){
+freshman + sophmore + junior + senior / 4;
+}
+
 };
-var car = {type:"Fiat", model:"500", color:"white"};
 
 
